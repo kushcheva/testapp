8df2a0b90b21259fb5fe32954eae6ce3fba716e7
kushcheva <tatyanakuscheva@gmail.com>
2023-02-15 01:19:16 +0200

    some edit

--- fp_app.py
+++ fp_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "This is main page version 1"
+    return "This is main page version 2. Welcome to my project"
 
 @app.route('/about')
 def about():
